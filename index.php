64ca41f65d701496f5e13456ea5222c42519c36e
Micho421 <michelelias333@hotmail.com>
2018-10-22 09:43:28 +0200

    gjort steg 5

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+
+
+
+    <form action="register.php" method="POST">
+        Name:<input type="text" name="name"><br>
+        Age:<input type="number" name="age" Mail:<input type="email" name="mail"><br>
+        Username: <input type="username" name="username"><br>
+        Password:<input type="password" name="password"><br>
+        Phone:<input type="tel" name="phone"><br>
+        <input type="submit">
+    </form>
+
+
+
+
+
+
+
+
+
 </body>
+
 </html>
